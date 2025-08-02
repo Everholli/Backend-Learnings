1db46354e90dc9d232aa9c9c84d1c76c309d50ed
Everholli <shrutimishra2522@gmail.com>
2025-08-02 10:45:10 +0530

    adding app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./.env"
@@ -10,10 +11,21 @@ dotenv.config({
 console.log("MONGODB_URL is: ", process.env.MONGODB_URL);
 
 
-connectDB();
-
-
-
+connectDB()
+.then( () => {
+    app.on("errors", (error) => {
+        console.log("ERROR: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port: ${process.env.PORT}`);
+        
+    })
+})
+.catch( (error) => {
+    console.log("MongoDB connection failed !!", error);
+    
+})
 
 
 
