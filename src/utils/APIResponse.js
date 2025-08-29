c8986935dd485cb2e3e54953bfdd28e3f43b90c5
Everholli <shrutimishra2522@gmail.com>
2025-08-29 11:58:47 +0530

    util code

---
+++ src/utils/APIResponse.js
@@ -0,0 +1,10 @@
+class APIResponse extends Error {
+    constructor(statuCode, data, messgae ="Success") {
+        this.statuCode = statuCode,
+        this.data = data,
+        this.message = messgae,
+        this.success = statuCode < 400
+    }
+}
+
+export { APIResponse }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -7,9 +7,10 @@ export { asyncHandler }
 
 
 
-
-
-
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {() => {}}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
 
 
 // const asyncHandler = (fn) => async (req, res, next) => {
